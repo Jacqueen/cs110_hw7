aaa852734577c7f2288b4908c8fa101a010d0f95
Jacqueen <31795720+Jacqueen@users.noreply.github.com>
2017-12-04 18:36:27 +0400

    Update Math.java

--- Homework7/src/utils/Math.java
+++ Homework7/src/utils/Math.java
@@ -1,13 +1,13 @@
 package utils;
 
 public class Math {
-    public static int factorial(int num) {
+    public static long factorial(int num) {
         if (num == 0)
             return 1;
         return num * factorial(num - 1);
 
     }
-    public static int factorialLoop (int num){
+    public static long factorialLoop (int num){
         int i = 1;
         while(num != 1) {
             i = i * num;
